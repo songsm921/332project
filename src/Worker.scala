99d4ac7368bc1dab05944e62f4a7cd2105815e6e
songsm921 <songsm921@postech.ac.kr>
2022-11-18 03:01:07 +0900

    fix: revise input specification

--- src/Worker.scala
+++ src/Worker.scala
@@ -2,104 +2,78 @@ import java.io._
 import scala.jdk.CollectionConverters._
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
-object fileIO{
-  val splitSizeMB = 32
-  val oneLineSize = 0.0001 /*100 MegaByte*/
-  val filename = "split"
-  var fileCount = 0
-  def readFileEachLine(path: String): Unit ={
-    for(line <- Source.fromFile(path).getLines()){
-      println(line)
-    }
+object Worker{
+  class IO(masterIP: String,outputPath: String){
+    def getMasterIP(): String = masterIP
+    def getOuputPath(): String = outputPath
   }
-  def writeFile(path: String) = {
-    new PrintWriter(path) {
-      write("Hello World!")
-      close
-
-    }
-  }
-  /*def splitFile(path: String,count: Int) = {
-    val lineCount = Source.fromFile(path).getLines().size
-    val iterCount = lineCount / count
-    val filename = "split"
-    val lines = ListBuffer[String]()
-    var i = 0
-    for(line <- Source.fromFile(path).getLines()){
-      lines.append(line)
-      i = i + 1
-      if(i % iterCount ==0 && i!=0){
-        new PrintWriter(filename + ((i / iterCount)-1).toString){
-          while(!lines.isEmpty){
-            write(lines.remove(0))
-            write("\n")
-            //lines.remove(0)
+  class Fragmentation{
+    private var fileCount = 0
+    private val splitSizeMB = 32
+    private val sizePerLineMB = 0.0001 // 10KB
+    private val fileNamePrefix = "partition"
+    def splitFile(path: String, sizeMB: Double = splitSizeMB): Unit = {
+      val lineCount = Source.fromFile(path).getLines().size
+      val rawFileSize = lineCount * sizePerLineMB
+      val linePerSplitedFile = sizeMB / sizePerLineMB
+      val lines = ListBuffer[String]()
+      var i = 0
+      if (rawFileSize < sizeMB) {
+        for (line <- Source.fromFile(path).getLines()) {
+          lines.append(line)
+        }
+        fileCount = fileCount + 1
+        new PrintWriter(fileNamePrefix + "." +(fileCount).toString) {
+          while (!lines.isEmpty) {
+            write(lines.remove(0) + "\r\n")
           }
           close
         }
       }
-    }
-    if(!lines.isEmpty){
-      new PrintWriter(filename + ((i/iterCount)).toString){
-        while(!lines.isEmpty){
-          write(lines.remove(0))
-          write("\n")
-        }
-        close
-      }
-    }
-  }*/
-  /* default = 32MB*/
-  def splitFile(path: String,sizeMB : Double = splitSizeMB) = {
-    val lineCount = Source.fromFile(path).getLines().size
-    val rawFileSize = lineCount * oneLineSize
-    val linePerSplitedFile = sizeMB / oneLineSize
-    val lines = ListBuffer[String]()
-    var i = 0
-    if(rawFileSize < sizeMB){
-      for(line <- Source.fromFile(path).getLines()){
-        lines.append(line)
-        }
-      fileCount = fileCount + 1
-      new PrintWriter(filename + (fileCount).toString) {
-        while (!lines.isEmpty) {
-          write(lines.remove(0)+"\r\n")
-        }
-        close
-      }
-    }
-    else{
-      for(line <- Source.fromFile(path).getLines()){
-        lines.append(line)
-        i = i + 1
-        if(i % linePerSplitedFile ==0 && i!=0){
-          fileCount = fileCount + 1
-          new PrintWriter(filename + (fileCount).toString){
-            while(!lines.isEmpty){
-              write(lines.remove(0)+"\r\n")
+      else {
+        for (line <- Source.fromFile(path).getLines()) {
+          lines.append(line)
+          i = i + 1
+          if (i % linePerSplitedFile == 0 && i != 0) {
+            fileCount = fileCount + 1
+            new PrintWriter(fileNamePrefix + "." + (fileCount).toString) {
+              while (!lines.isEmpty) {
+                write(lines.remove(0) + "\r\n")
+              }
+              close
+            }
+          }
 
+        }
+        if (!lines.isEmpty) {
+          fileCount = fileCount + 1
+          new PrintWriter(fileNamePrefix + "." + fileCount) {
+            while (!lines.isEmpty) {
+              write(lines.remove(0) + "\r\n")
             }
             close
           }
         }
-
-      }
-      if(!lines.isEmpty){
-        fileCount = fileCount + 1
-        new PrintWriter(filename + fileCount){
-          while(!lines.isEmpty){
-            write(lines.remove(0)+"\r\n")
-          }
-          close
-        }
       }
     }
-
   }
+
   def main(args: Array[String]) : Unit = {
-    for(arg<-args) {
-      splitFile(arg)
+    val argList = ListBuffer(args: _ *)
+    val instFragment = new Fragmentation()
+    val masterIP = argList.remove(0)
+    argList.remove(0)
+    while(argList.head != "-O"){
+      instFragment.splitFile(argList.remove(0))
     }
+    argList.remove(0)
+    val outputPath = argList.remove(0)
+    val instIO = new IO(masterIP,outputPath)
+    print(instIO.getMasterIP())
+    print(instIO.getOuputPath())
   }
 
 }
+
+
+// 192,fff -I path1 path2 .... -O Outputpath
